1330fe3cd588f0902e46461e616060e8531cf612
Sam Bencivengo <sambencivengo@gmail.com>
2022-01-16 18:42:38 -0500

    created listing resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
